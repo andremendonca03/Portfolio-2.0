009d75894324262649bef5c908ce7d1d4509c89f
andremendonca03 <andremendonca.033@gmail.com>
2024-02-06 23:04:41 +1000

    📣 Feat: Adding Shadcn UI and tooltip

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,16 +3,40 @@
 import Link from "next/link";
 import LogoIcon from "../icons/LogoIcon";
 import LampIcon from "../icons/LampIcon";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+import React from "react";
 
 const Header = () => {
+
+  const [currentTheme, setCurrentTheme] = React.useState("Dev Mode");
+
+  function handleThemeChange() {
+    const html = document.documentElement;
+    if (html) {
+      html.classList.toggle("devMode");
+      html.classList.toggle("designMode");
+
+      if (html.classList.contains("devMode")) {
+        setCurrentTheme("Design Mode");
+      } else {
+        setCurrentTheme("Dev Mode");
+      }
+    }
+  }
+
   return (
-    <header className="sticky top-0 bg-n-darkBg ">
+    <header className="sticky top-0 bg-r-bg td-150 ">
       <div className="container flex justify-between items-center py-3">
         <Link href="/" className="hover:scale-110 transition duration-150">
           <LogoIcon />
         </Link>
 
-        <nav className="flex gap-5 font-large text-n-white">
+        <nav className="flex gap-5 font-large text-r-text">
           <Link href="#about-me" scroll={false}>About me</Link>
           <Link href="#skills" scroll={false}>Skills</Link>
           <Link href="#showcase" scroll={false}>Showcase</Link>
@@ -20,12 +44,20 @@ const Header = () => {
         </nav>
 
         <div>
-          <LampIcon className="cursor-pointer" onClick={() => {
-            if (document.documentElement) {
-              document.documentElement.classList.toggle("devMode");
-              document.documentElement.classList.toggle("designMode");
-            }
-          }} />
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <LampIcon className="cursor-pointer [html.devMode_&>.inside]:fill-none [html.designMode_&>.stroke]:fill-r-text"
+                onClick={handleThemeChange} />
+              </TooltipTrigger>
+
+              <TooltipContent>
+                <p>{currentTheme}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+
         </div>
       </div>
     </header>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,7 +74,21 @@ const config: Config = {
         1: '1',
         2: '2',
         5: '5',
-      }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
   plugins: [],
