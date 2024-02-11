a2c73fb868396f259aba53ca5d6b72a54590f722
andremendonca03 <andremendonca.033@gmail.com>
2024-02-11 11:16:05 +1000

    📣 Feat: Footer Component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import LinkedinIcon from '../icons/LinkedinIcon';
+import GithubIcon from '../icons/GithubIcon';
+import DribbbleIcon from '../icons/DribbbleIcon';
+
+const Footer = () => {
+
+  return (
+    <footer className='container py-6'>
+      <h2 className='font-heading4 text-r-text mb-2'>Get in touch</h2>
+      <p className='text-r-text-muted mb-3' >Want to know more about me or have any enquiries? Let’s chat!</p>
+
+      <p className='text-r-text-muted'>Shout me an email at <a href="mailto:andremendonca.033@gmail.com" className="inline font-bold text-r-text">andremendonca.033@gmail.com</a></p>
+      <p className="text-r-text-muted mb-5">Or drop me a call at <a href="tel:+61451009412" className="inline font-bold text-r-text">+61 451 009 412</a></p>
+
+      <p className='text-r-text-muted mb-4'>You can also find more about me and my work on the links below:</p>
+
+      <div className="flex items-center gap-4">
+        <a href="" className="group rounded bg-r-accent p-2 border-2 border-r-accent hover:bg-transparent">
+          <LinkedinIcon className='size-[18px] group-hover:[&>path]:fill-r-text' />
+        </a>
+
+        <a href="" className="group rounded bg-r-accent p-2 border-2 border-r-accent hover:bg-transparent">
+          <GithubIcon className='size-[18px] group-hover:[&>path]:fill-r-text' />
+        </a>
+
+        <a href="" className="group rounded bg-r-accent p-2 border-2 border-r-accent hover:bg-transparent">
+          <DribbbleIcon className='size-[18px] group-hover:[&>path]:fill-r-text' />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
