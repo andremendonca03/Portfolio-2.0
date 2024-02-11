9f6a8bbe2447a92c0ceb866ef97b57e631a6217a
andremendonca03 <andremendonca.033@gmail.com>
2024-02-11 11:17:12 +1000

    📣 Feat: Sidebar Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
+import About from '@/components/about/About';
+import Footer from '@/components/footer/Footer';
 import Header from '@/components/header/Header';
+import IntroSection from '@/components/introSection/IntroSection';
+import Sidebar from '@/components/sidebar/Sidebar';
+import SkillAreas from '@/components/skillAreas/SkillAreas';
+import Strip from '@/components/strip/Strip';
 
 
 
 const Homepage = () => {
+
   return (
     <>
       <Header />
     
       <main className='main'>
-
+        <IntroSection />
+        <About />
+        <SkillAreas />
       </main>
     
+      <Footer />
+      <Sidebar />
     </>
   )
 }
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,29 @@
+import DribbbleIcon from "../icons/DribbbleIcon";
+import GithubIcon from "../icons/GithubIcon";
+import LinkedinIcon from "../icons/LinkedinIcon";
+import MailIcon from "../icons/MailIcon";
+
+const Sidebar = () => {
+
+  return (
+    <nav className="fixed right-0 top-1/2 -translate-y-1/2 flex flex-col gap-4 bg-r-bgGlassy backdrop-blur-lg py-4 px-3 rounded-tl rounded-bl td-150 z-10">
+      <a href="#" className="group p-1">
+        <LinkedinIcon className="size-[18px] [&>path]:fill-r-text group-hover:[&>path]:fill-[#0E76A8] [&>path]:td-150" />
+      </a>
+
+      <a href="#" className="group p-1">
+        <GithubIcon className="size-[18px] [&>path]:fill-r-text group-hover:[&>path]:fill-n-grey [&>path]:td-150" />
+      </a>
+
+      <a href="#" className="group p-1">
+        <DribbbleIcon className="size-[18px] [&>path]:fill-r-text group-hover:[&>path]:fill-[#D8BAF1] [&>path]:td-150" />
+      </a>
+
+      <a href="#" className="group p-1">
+        <MailIcon className="size-[18px] [&>path]:fill-r-text group-hover:[&>path]:fill-r-accent [&>path]:td-150" />
+      </a>
+    </nav>
+  );
+}
+
+export default Sidebar;
