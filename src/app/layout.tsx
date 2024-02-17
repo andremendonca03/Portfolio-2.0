4a884382513325985d1156d4b494880b070bcf81
andremendonca03 <andremendonca.033@gmail.com>
2024-02-17 12:22:59 +1000

    📣 Feat: Animated Background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,47 @@
-import type { Metadata } from 'next';
-import { Inria_Sans, Fira_Code } from 'next/font/google';
-import '@/styles/globals.scss';
+import type { Metadata } from "next";
+import { Inria_Sans, Fira_Code } from "next/font/google";
+import "@/styles/globals.scss";
 import { GlobalStorage } from "@/contexts/GlobalContext";
 
-import cn from '@/constants/filters/cn';
+import cn from "@/constants/filters/cn";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 
-const inriaSans = Inria_Sans({ subsets: ['latin'], weight: ["400", "700"], variable: "--font-inria-sans"});
-const firaCode = Fira_Code({subsets: ['latin'], weight: "variable", variable: "--font-fira-code"});
+const inriaSans = Inria_Sans({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-inria-sans",
+});
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  weight: "variable",
+  variable: "--font-fira-code",
+});
 
 export const metadata: Metadata = {
-  title: 'Andre Mendonca - UX/UI Designer & Front-end Developer',
-  description: '-',
-}
+  title: "Andre Mendonca - UX/UI Designer & Front-end Developer",
+  description: "-",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <GlobalStorage>
-      <html lang="en-AU" className={cn("devMode", firaCode.variable, inriaSans.variable)}>
-        <body className={cn("bg-r-bg text-r-text font-copy relative td-150", inriaSans.className)}>
-          <div className="bg-r-bg relative td-150">
-            {children}
-          </div>
+      <html
+        lang="en-AU"
+        className={cn("devMode", firaCode.variable, inriaSans.variable)}
+      >
+        <body
+          className={cn(
+            "bg-r-bg text-r-text font-copy relative td-150",
+            inriaSans.className
+          )}
+        >
+          {children}
         </body>
       </html>
     </GlobalStorage>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,61 @@
-import About from '@/components/about/About';
-import Footer from '@/components/footer/Footer';
-import Header from '@/components/header/Header';
-import IntroSection from '@/components/introSection/IntroSection';
-import Sidebar from '@/components/sidebar/Sidebar';
-import SkillAreas from '@/components/skillAreas/SkillAreas';
-import Strip from '@/components/strip/Strip';
+"use client";
 
-
+import About from "@/components/about/About";
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import IntroSection from "@/components/introSection/IntroSection";
+import Sidebar from "@/components/sidebar/Sidebar";
+import SkillAreas from "@/components/skillAreas/SkillAreas";
+import Strip from "@/components/strip/Strip";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
+import { GlobalContext } from "@/contexts/GlobalContext";
+import React from "react";
 
 const Homepage = () => {
+  const global = React.useContext(GlobalContext);
 
   return (
-    <>
-      <Header />
-    
-      <main className='main'>
-        <IntroSection />
-        <About />
-        <SkillAreas />
-      </main>
-    
-      <Footer />
-      <Sidebar />
-    </>
-  )
-}
+    <div className="bg-r-bg relative td-150">
+      {global.themeMode === "devMode" ? (
+        <BackgroundGradientAnimation
+          size="38%"
+          interactive={false}
+          gradientBackgroundStart="rgb(17, 17, 17)"
+          gradientBackgroundEnd="rgb(17,17,17)"
+          firstColor={"73, 59, 227"}
+          secondColor={"83, 69, 245"}
+          thirdColor={"73, 59, 227"}
+          fourthColor={"83, 69, 245"}
+          fifthColor={"73, 59, 227"}
+        />
+      ) : (
+        <BackgroundGradientAnimation
+          size="38%"
+          interactive={false}
+          gradientBackgroundStart="rgb(256, 256, 256)"
+          gradientBackgroundEnd="rgb(256,256,256)"
+          firstColor={"227, 68, 47"}
+          secondColor={"245, 69, 69"}
+          thirdColor={"227, 68, 47"}
+          fourthColor={"245, 69, 69"}
+          fifthColor={"227, 68, 47"}          
+        />
+      )}
+
+      <div className="relative z-1 inset-0 w-full h-full">
+        <Header />
+
+        <main className="main">
+          <IntroSection />
+          <About />
+          <SkillAreas />
+        </main>
+
+        <Footer />
+        <Sidebar />
+      </div>
+    </div>
+  );
+};
 
 export default Homepage;
